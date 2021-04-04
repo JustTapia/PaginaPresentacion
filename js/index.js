b68e2bc85512687447131ff1dcdb77c59f8c835c
JustTapia <38672129+JustTapia@users.noreply.github.com>
2021-04-04 01:19:02 -0600

    Corregido Bug de href en mobil

--- js/index.js
+++ js/index.js
@@ -1,48 +1,46 @@
 document.addEventListener('DOMContentLoaded', init);
-
 let elementoActualnum = 1;
 let elementoActual = document.getElementById("stepperAboutId");
 let actualWindowWidth = screen.width;
+function init() {
+    let stepperAbout = document.getElementById("stepperAboutId");
+    stepperAbout.addEventListener("click", animacionA1);
 
-function init(){
-    let btnAbout = document.getElementById("stepperAboutId");
-    btnAbout.addEventListener("click", animacionA1);
+    let stepperResume = document.getElementById("stepperResumeId");
+    stepperResume.addEventListener("click", animacionA2);
 
-    let btnPortfolio = document.getElementById("stepperPortfolioId");
-    btnPortfolio.addEventListener("click", animacionA2);
+    let stepperPortfolio = document.getElementById("stepperPortfolioId");
+    stepperPortfolio.addEventListener("click", animacionA3);
 
-    let btnResume = document.getElementById("stepperResumeId");
-    btnResume.addEventListener("click", animacionA3);
+    let stepperInterests = document.getElementById("stepperInterestsId");
+    stepperInterests.addEventListener("click", animacionA4);
 
-    let btnInterests = document.getElementById("stepperInterestsId");
-    btnInterests.addEventListener("click", animacionA4);
+    let stepperContact = document.getElementById("stepperContactId");
+    stepperContact.addEventListener("click", animacionA5);
+    elementoActual = stepperAbout;
 
-    let btnContact = document.getElementById("stepperContactId");
-    btnContact.addEventListener("click", animacionA5);
-    elementoActual = btnAbout;
-
-    if(screen.width <= 992){
+    if (screen.width <= 992) {
         cartas = document.getElementsByClassName("carta");
         Array.from(cartas).forEach(carta => {
             carta.classList.remove("hidden");
         });
     };
     window.onresize = () => {
-        if(screen.width === actualWindowWidth){
+        if (screen.width === actualWindowWidth) {
             return;
-        }else if(screen.width <= 992){
+        } else if (screen.width <= 992) {
             cartas = document.getElementsByClassName("carta");
             Array.from(cartas).forEach(carta => {
                 carta.classList.remove("hidden");
                 carta.removeAttribute("style");
             });
             actualWindowWidth = screen.width;
-        }else{
+        } else {
             cartas = document.getElementsByClassName("carta");
             Array.from(cartas).forEach(carta => {
-                if(carta !== cartas[elementoActualnum-1]){
+                if (carta !== cartas[elementoActualnum - 1]) {
                     carta.classList.add("hidden");
-                }else{
+                } else {
                     carta.style.animation = "fadein-bottom 0.5s linear 1 both";
                 }
             });
@@ -54,8 +52,8 @@ function init(){
     }
 };
 
-function animacionA1(){
-    if(animacion(1)){
+function animacionA1() {
+    if (animacion(1)) {
         elementoActualnum = 1;
         let elementoNuevo = document.getElementById("stepperAboutId");
         elementoActual.getElementsByClassName("stepper_link")[0].classList.remove("stepper_link--active");
@@ -64,19 +62,9 @@ function animacionA1(){
     }
 }
 
-function animacionA2(){
-    if(animacion(2)){
+function animacionA2() {
+    if (animacion(2)) {
         elementoActualnum = 2;
-        let elementoNuevo = document.getElementById("stepperPortfolioId");
-        elementoActual.getElementsByClassName("stepper_link")[0].classList.remove("stepper_link--active");
-        elementoNuevo.getElementsByClassName("stepper_link")[0].classList.add("stepper_link--active");
-        elementoActual = elementoNuevo;
-    }
-}
-
-function animacionA3(){
-    if(animacion(3)){
-        elementoActualnum = 3;
         let elementoNuevo = document.getElementById("stepperResumeId");
         elementoActual.getElementsByClassName("stepper_link")[0].classList.remove("stepper_link--active");
         elementoNuevo.getElementsByClassName("stepper_link")[0].classList.add("stepper_link--active");
@@ -84,8 +72,18 @@ function animacionA3(){
     }
 }
 
-function animacionA4(){
-    if(animacion(4)){
+function animacionA3() {
+    if (animacion(3)) {
+        elementoActualnum = 3;
+        let elementoNuevo = document.getElementById("stepperPortfolioId");
+        elementoActual.getElementsByClassName("stepper_link")[0].classList.remove("stepper_link--active");
+        elementoNuevo.getElementsByClassName("stepper_link")[0].classList.add("stepper_link--active");
+        elementoActual = elementoNuevo;
+    }
+}
+
+function animacionA4() {
+    if (animacion(4)) {
         elementoActualnum = 4;
         let elementoNuevo = document.getElementById("stepperInterestsId");
         elementoActual.getElementsByClassName("stepper_link")[0].classList.remove("stepper_link--active");
@@ -94,8 +92,8 @@ function animacionA4(){
     }
 }
 
-function animacionA5(){
-    if(animacion(5)){
+function animacionA5() {
+    if (animacion(5)) {
         elementoActualnum = 5;
         let elementoNuevo = document.getElementById("stepperContactId");
         elementoActual.getElementsByClassName("stepper_link")[0].classList.remove("stepper_link--active");
@@ -105,32 +103,32 @@ function animacionA5(){
 }
 
 
-function animacion(elementoNuevoNum){
-    if (elementoNuevoNum>elementoActualnum) {
-        let actual = document.getElementById("carta"+ elementoActualnum);
+function animacion(elementoNuevoNum) {
+    if (elementoNuevoNum > elementoActualnum) {
+        let actual = document.getElementById("carta" + elementoActualnum);
         actual.style.animation = "fadeout-top 0.5s linear 1 both";
-        let nuevo = document.getElementById("carta"+ elementoNuevoNum);
+        let nuevo = document.getElementById("carta" + elementoNuevoNum);
         nuevo.style.animation = "fadein-top 0.5s linear 1 both";
         setTimeout(hidden, 100, actual);
-        setTimeout(display, 100, nuevo); 
+        setTimeout(display, 100, nuevo);
         return true;
 
-    } else if(elementoNuevoNum<elementoActualnum){
-        let actual = document.getElementById("carta"+ elementoActualnum);
+    } else if (elementoNuevoNum < elementoActualnum) {
+        let actual = document.getElementById("carta" + elementoActualnum);
         actual.style.animation = "fadeout-bottom 0.5s linear 1 both";
-        let nuevo = document.getElementById("carta"+ elementoNuevoNum);
+        let nuevo = document.getElementById("carta" + elementoNuevoNum);
         nuevo.style.animation = "fadein-bottom 0.5s linear 1 both";
         setTimeout(hidden, 100, actual);
-        setTimeout(display, 100, nuevo); 
+        setTimeout(display, 100, nuevo);
         return true;
     };
     return false;
 };
 
-function hidden(aEsconder){
+function hidden(aEsconder) {
     aEsconder.classList.add('hidden');
 };
 
-function display(aMostrar){
+function display(aMostrar) {
     aMostrar.classList.remove('hidden');
 };
